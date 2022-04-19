b61c8d0d0656e7d25ad4fb99b987a7d8cece2c48
Restyled.io <commits@restyled.io>
2022-04-19 08:44:37 +0000

    Restyled by clang-format

--- old/static-jekyll-archive/_includes/script-download-icon.js
+++ old/static-jekyll-archive/_includes/script-download-icon.js
@@ -2,18 +2,15 @@
 //
 
 (function() {
-	var removeSuccess;
-	
-	removeSuccess = function() {
-		return $('.button').removeClass('success');
-	};
-	
-	// After 5 seconds, back return to normal
-	$(document).ready(function() {
-		return $('.button').click(function() {
-    		$(this).addClass('success');
-    		return setTimeout(removeSuccess, 5000);
-		});
-	});
+var removeSuccess;
 
+removeSuccess = function() { return $('.button').removeClass('success'); };
+
+// After 5 seconds, back return to normal
+$(document).ready(function() {
+  return $('.button').click(function() {
+    $(this).addClass('success');
+    return setTimeout(removeSuccess, 5000);
+  });
+});
 }).call(this);
