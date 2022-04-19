75bf5f660806caafd9a637d06fb91ae1baf9df9c
Restyled.io <commits@restyled.io>
2022-04-19 08:44:38 +0000

    Restyled by prettier

--- old/static-jekyll-archive/_includes/script-download-icon.js
+++ old/static-jekyll-archive/_includes/script-download-icon.js
@@ -1,16 +1,18 @@
 // script-download-icon.js
 //
 
-(function() {
-var removeSuccess;
+(function () {
+  var removeSuccess;
 
-removeSuccess = function() { return $('.button').removeClass('success'); };
+  removeSuccess = function () {
+    return $(".button").removeClass("success");
+  };
 
-// After 5 seconds, back return to normal
-$(document).ready(function() {
-  return $('.button').click(function() {
-    $(this).addClass('success');
-    return setTimeout(removeSuccess, 5000);
+  // After 5 seconds, back return to normal
+  $(document).ready(function () {
+    return $(".button").click(function () {
+      $(this).addClass("success");
+      return setTimeout(removeSuccess, 5000);
+    });
   });
-});
-}).call(this);
+}.call(this));
